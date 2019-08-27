957c75fdede56cd6332144e7a807c3cc288a9491
Shubham Gupta <shum979@gmail.com>
2019-08-27 19:19:15 +0100

    changed

--- src/main/java/spark/JavaWordCount.java
+++ src/main/java/spark/JavaWordCount.java
@@ -23,30 +23,10 @@ public final class JavaWordCount {
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         JavaRDD<String> lines = ctx.textFile(args[0], 1);
 
-        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterable<String> call(String s) {
-                return Arrays.asList(SPACE.split(s));
-            }
-        });
 
-        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String s) {
-                return new Tuple2<>(s, 1);
-            }
-        });
-
-        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer i1, Integer i2) {
-                return i1 + i2;
-            }
-        });
-
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?, ?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
+        List<String> output = lines.collect();
+        for (String tuple : output) {
+            System.out.println(tuple + ": ");
         }
         ctx.stop();
     }
