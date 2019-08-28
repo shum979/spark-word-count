124dcfd20bfd9bb39ffb4c73bbc0d15d7f7c1480
Shubham Gupta <shum979@gmail.com>
2019-08-28 10:17:25 +0100

    added souts to track progess

--- src/main/java/spark/JavaWordCount.java
+++ src/main/java/spark/JavaWordCount.java
@@ -1,12 +1,11 @@
 package spark;
 
-import java.util.Arrays;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 import java.util.List;
 import java.util.regex.Pattern;
-import org.apache.spark.api.java.*;
-import org.apache.spark.api.java.function.*;
-import org.apache.spark.SparkConf;
-import scala.Tuple2;
 
 public final class JavaWordCount {
 
@@ -14,20 +13,31 @@ public final class JavaWordCount {
 
     public static void main(String[] args) throws Exception {
 
+        System.out.println("Test spark application started .......");
+        System.out.println("SPark on openShift started .......");
+
         if (args.length < 1) {
             System.err.println("Usage: JavaWordCount <file>");
             System.exit(1);
         }
 
+        System.out.println("Program argument received ... "+ args[0]);
+
         SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+
+        System.out.println("Java spark context started .....");
+
         JavaRDD<String> lines = ctx.textFile(args[0], 1);
 
+        System.out.println("spark read file completed .....");
 
         List<String> output = lines.collect();
         for (String tuple : output) {
             System.out.println(tuple + ": ");
         }
+
+        System.out.println("Files lines collected and printed .....");
         ctx.stop();
     }
 }
